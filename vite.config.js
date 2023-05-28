3dd45439c66d0c6ccfda9bba78fc2a8d359501e5
versedpro <ultraj0330@gmail.com>
2023-05-28 10:56:08 +0800

    chore: Modify API url

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'https://stakeminttradingbot1.tail135b9.ts.net',
         changeOrigin: true,
       },
     },
